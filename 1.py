7aebd4a2baf7a993e11c34b1df9ab81c36ee1400
Saksham1309 <sakshammittal502@gmail.com>
2025-07-27 13:57:52 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+# # Assignment 1
+num1=float(input("Enter 1st number: "))
+num2=float(input("Enter 2nd number: "))
+sum=num1+num2
+differ=num2-num1
+prod=num1*num2
+quot=num2/num1
+print ("Addition: ", sum)
+print ("Subtraction: ", differ)
+print ("Multiplication: ", prod)
+print ("Division: ", quot)
+
+# Assignment 2
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+print("Hello,", name1, name2 + "!", "Welcome to the Python program.")
+
